5b49860a2352f6676af2409b9b688e92bd4bff4b
Matvey Dranitsyn <92252237+DMatvei@users.noreply.github.com>
2022-10-03 13:06:49 +1000

    Create counter button

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var counterView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        counterView = findViewById(R.id.CounterView)
+    }
+
+    fun ClickButtonUpdate (view: View){
+        val countString = counterView.text.toString()
+
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        counterView.text = count.toString()
     }
 }
+
